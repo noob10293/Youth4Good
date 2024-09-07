0918a0f5c15dace0f8c0e8f4920a0b9dc05f8e33
ianchen10293 <ianchen10293@gmail.com>
2024-09-07 21:00:30 +0000

    commit

--- index.js
+++
@@ -1,8 +0,0 @@
-// Function to get a specific cookie by name
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-  
-// Adju
