d88792a89766bca38038156ffe8b485d4a7c94f5
ianchen10293 <ianchen10293@gmail.com>
2024-09-07 18:12:17 +0000

    untitled

--- index.js
+++ index.js
@@ -7,46 +7,45 @@ app.get('/', (req, res) => {
 });
 
 // Chat Room
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+// const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
 
-  ws.on('message', function (message) {
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message);
+//   ws.on('message', function (message) {
+//       wss.clients.forEach((client) => {
+//         if (client !== ws && client.readyState === WebSocket.OPEN) {
+//           client.send(message);
 
-        }
-      });
-    });
-});
-const chatContainer = document.getElementById('chat-container');
-const messageInput = document.getElementById('message-input');
-const sendBtn = document.getElementById('send-btn');
+//         }
+//       });
+//     });
+// });
+// const chatContainer = document.getElementById('chat-container');
+// const messageInput = document.getElementById('message-input');
+// const sendBtn = document.getElementById('send-btn');
 
-const   
- socket = new WebSocket('ws://your-server-url');
+// const socket = new WebSocket('ws://your-server-url');
 
-socket.onopen = () => {
-  console.log('Connected to WebSocket server');
-};
+// socket.onopen = () => {
+//   console.log('Connected to WebSocket server');
+// };
 
-socket.onmessage = (event) => {
-  const message = JSON.parse(event.data);   
+// socket.onmessage = (event) => {
+//   const message = JSON.parse(event.data);   
 
-  const messageDiv = document.createElement('div');
-  messageDiv.textContent = message;
-  chatContainer.appendChild(messageDiv);
-};
+//   const messageDiv = document.createElement('div');
+//   messageDiv.textContent = message;
+//   chatContainer.appendChild(messageDiv);
+// };
 
-sendBtn.addEventListener('click', () => {
-  const message = messageInput.value;
-  socket.send(JSON.stringify(message));
-  messageInput.value = '';
-});
+// sendBtn.addEventListener('click', () => {
+//   const message = messageInput.value;
+//   socket.send(JSON.stringify(message));
+//   messageInput.value = '';
+// });
 
 const port = parseInt(process.env.PORT) || 3000;
 app.listen(port, () => {
