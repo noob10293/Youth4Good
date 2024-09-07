6bbe944a260f32b958fc730fa6f730d2e6f9681c
ianchen10293 <ianchen10293@gmail.com>
2024-09-07 22:10:06 +0000

    connected links

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 // Adjust UI based on login state
+
+console.log("HI")
 document.addEventListener('DOMContentLoaded', () => {
     const loggedin = getCookie('loggedin');
 
     // Hide login link if user is logged in
     const loginLink = document.getElementById('login');
     const logout = document.getElementById('logout');
-    if (loginLink && loggedin) {
+    if (loggedin) {
+        console.log("HI")
         loginLink.style.display = 'none';
         logout.style.display = 'block';
     }
