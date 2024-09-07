196db7781930bac5a00c5149a9fc5db04900d741
ianchen10293 <ianchen10293@gmail.com>
2024-09-07 18:13:05 +0000

    untitled

--- index.js
+++ index.js
@@ -51,3 +51,7 @@ const port = parseInt(process.env.PORT) || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
